43ec1b08bd462973cac22b3e770822d80ccc4b74
MEHDI BELLAM <mehdi.bellam@outlook.fr>
2023-01-29 12:24:32 +0100

    :rocket: importing database in index :rocket:

--- index.js
+++ index.js
@@ -4,8 +4,10 @@ const express = require("express");
 const cors = require("cors");
 const { Server } = require("socket.io");
 require("dotenv").config();
-const mongoose = require("mongoose");
-const { Schema } = mongoose;
+
+const db = require("./database.js");
+
+
 // APP -----------------------------------------------------------------------------------------------------------------
 const app = express();
 const httpServer = http.createServer(app);
@@ -15,56 +17,9 @@ const ioServer = new Server(httpServer, {
   },
 });
 
-// CONNECTION TO DATABASE _____________________________________________________________________________________________________________________________________________
-
-// var username = encodeURIComponent(process.env.MONGO_DB_ATLAS_LOGIN_USERNAME);
-// var password = encodeURIComponent(process.env.MONGO_DB_ATLAS_LOGIN_PASSWORD);
-var link =
-  "mongodb+srv://ImDImeh:projetnodejs2023@pixelchat.hfbyom1.mongodb.net/?retryWrites=true&w=majority";
-  
-mongoose.connect(
-  link, { useNewUrlParser: true, useUnifiedTopology: true })
-  .then(() => console.log("Connected to MongoDB..."));
+db.GetAllUser();
 
 
-// ADD COLLECTION TO DATABASE _____________________________________________________________________________________________________________________________________________
-
-const USER = new Schema({
-  FisrtName: String,
-  LastName: String,
-  Password: String,
-  Pseudo: String,
-});
-const User = mongoose.model("User", USER);
-
-const message = new Schema({
-  message: String,
-  author_id: [{ type: Schema.Types.ObjectId, ref: "User" }],
-  body: String,
-  DateTime: Number,
-});
-
-
-const Message = mongoose.model("Message", message);
-
-const UserTest = new User({
-  FisrtName: "Test",
-  LastName: "Test",
-  Password: "Test",
-  Pseudo: "Test",
-});
-UserTest.save();
-
-
-MessageTest = new Message({
-  message: "Voici un message de test",
-  author_id: UserTest._id,
-  body: "Voici un message de test un peu l=plus long pour voir si ca marche",
-  DateTime: Date.now(),
-
-  
-});
-MessageTest.save();
 // MIDDLEWARE ----------------------------------------------------------------------------------------------------------
 app.use(cors());
 app.use(express.json());
@@ -85,6 +40,6 @@ ioServer.on("connection", (socket) => {
 // SERVER --------------------------------------------------------------------------------------------------------------
 httpServer.listen(3000, () => {
   console.log("Server listening on port 3000");
-  console.log(link);
+ 
 
 });
