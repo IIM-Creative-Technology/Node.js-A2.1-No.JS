9a2c12d9a52e64ad8245edda2892cd0ca9468226
Félix Laviéville <lavieville.felix@gmail.com>
2023-01-29 19:11:54 +0100

    removing useless folder

--- index.js
+++ index.js
@@ -96,6 +96,4 @@ ioServer.on("connection", (socket) => {
 // SERVER --------------------------------------------------------------------------------------------------------------
 httpServer.listen(3000, () => {
   console.log("Server listening on port 3000");
- 
-
 });
