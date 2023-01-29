1f61b01ad9b406b26bba9fd22de8eef31be86d39
Vorn-Hagen <thibaut.francois@edu.devinci.fr>
2023-01-29 19:31:18 +0100

    My contribution

--- front-end/script.js
+++ front-end/script.js
@@ -1,1 +1,5 @@
 const socket = io("http://localhost:3000")
+
+const login = document.getElementById("login")
+const disco = document.getElementById("disco")
+const secret = document.getElementById("secret")
--- index.js
+++ index.js
@@ -3,6 +3,7 @@ const http = require('http');
 const express = require('express');
 const cors = require('cors');
 const {Server} = require('socket.io');
+const jwt = require("jsonwebtoken");
 
 // APP -----------------------------------------------------------------------------------------------------------------
 const app = express();
@@ -13,6 +14,9 @@ const ioServer = new Server(httpServer, {
     }
 });
 
+// VAR -----------------------------------------------------------------------------------------------------------------
+const secretKey = "secret-key";
+
 // MIDDLEWARE ----------------------------------------------------------------------------------------------------------
 app.use(cors());
 app.use(express.json());
@@ -22,6 +26,56 @@ app.get('/', (req, res) => {
     res.send('Hello World!');
 });
 
+
+
+app.post("/login", (req, res) => {
+    const { username, password } = req.body;
+
+    // Authentification
+    //const user = authenticateUser(username, password);
+    const user = true;
+
+    const token = jwt.sign({ user }, secretKey, { expiresIn: "1h" });
+    res.json({ message: "Login successful", "token":token });
+
+    // if (user) {
+    //     const token = jwt.sign({ user }, secretKey, { expiresIn: "1h" });
+    //     res.send({ message: "Login successful", token });
+    // } else {
+    //     res.status(401).send({ message: "Incorrect username or password" });
+    // }
+});
+
+app.post("/logout", (req, res) => {
+    // You do not need to implement logout in JWT-based authentication
+    res.send({ message: "Logout successful", destroyToken : true });
+});
+
+
+app.use((req, res, next) => {
+    const token = req.header("Authorization");
+    if (!token) {
+    return res.status(401).send({ message: "Access denied" });
+    }
+    
+    try {
+        const decoded = jwt.verify(token, secretKey);
+        req.user = decoded.user;
+        next();
+    } catch (error) {
+        res.status(400).send({ message: "Invalid token" });
+    }
+});
+
+
+
+
+
+
+
+
+
+
 // SOCKET.IO -----------------------------------------------------------------------------------------------------------
 ioServer.on('connection', (socket) => {
     console.log('User connected');
@@ -29,6 +83,9 @@ ioServer.on('connection', (socket) => {
         console.log('User disconnected');
     });
 });
+
+
+
 
 // SERVER --------------------------------------------------------------------------------------------------------------
 httpServer.listen(3000, () => {
