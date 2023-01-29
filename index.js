4f1756df5fb8c96063df2c85c682a2cda6897e6d
Félix Laviéville <lavieville.felix@gmail.com>
2023-01-29 12:07:30 +0100

    Removing no-usage routes & updating Doc

--- index.js
+++ index.js
@@ -28,15 +28,15 @@ app.get('/user/:id', (req, res) => {
     res.send('Lecture d un user');
 });
 
-app.put('/user/:id', (req, res) => {
-    res.send('mise à jour d un user');
-});
-
 app.post('/user', (req, res) => {
     res.send('insertion d un user');
 });
 
-app.delete('/user/:Id', (req, res) => {
+app.put('/user/:id', (req, res) => {
+    res.send('mise à jour d un user');
+});
+
+app.delete('/user/:id', (req, res) => {
     res.send('suppression d un user');
 });
 
@@ -49,31 +49,23 @@ app.get('/message/:id', (req, res) => {
     res.send('Lecture d un message');
 });
 
-app.put('/message/:id/user/:id', (req, res) => {
-    res.send('mise à jour d un message');
-});
-
-app.post('/message/user/:id', (req, res) => {
+app.post('/message/:id', (req, res) => {
     res.send('insertion d un message');
 });
 
 app.delete('/message/:id', (req, res) => {
     res.send('suppression d un message');
 });
-/* routes pixels */
-/* x, y, userid, color */
 
+/* routes pixels */
 app.get('/pixel', (req, res) => {
-    res.send('Lecture d un pixel');
+    res.send('Lecture de la grille de pixels');
 });
 
-app.put('/pixel/:id/user/:id', (req, res) => {
+app.put('/pixel', (req, res) => {
     res.send('mise à jour d un pixel');
 });
 
-app.post('/pixel/user/:id', (req, res) => {
-    res.send('creation d un pixel');
-});
 // SOCKET.IO -----------------------------------------------------------------------------------------------------------
 ioServer.on('connection', (socket) => {
     console.log('User connected');
